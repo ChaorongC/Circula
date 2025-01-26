494729dff04e7c345b98d8454beecdda273c019e
crchen12 <108202347+ChaorongC@users.noreply.github.com>
2025-01-26 15:48:34 -0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "Basic Sphinx Example Project"
-copyright = "2022, Read the Docs core team"
-author = "Read the Docs core team"
+project = "Circula"
+copyright = "2025, Chaorong Chen, Wei Li Lab"
+author = "Chaorong Chen"
 
 
 # -- General configuration ---------------------------------------------------
